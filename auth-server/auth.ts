95c51df17f5b6186270c303f13db8f3c1cc30ba6
Nikita <github@nikiv.dev>
2025-01-11 00:52:04 +0100

    auth server

---
+++ auth-server/auth.ts
@@ -0,0 +1,47 @@
+// sets up openauth server
+// as per docs: https://openauth.js.org/docs
+import { issuer } from "@openauthjs/openauth"
+import { MemoryStorage } from "@openauthjs/openauth/storage/memory"
+import { PasswordProvider } from "@openauthjs/openauth/provider/password"
+import { PasswordUI } from "@openauthjs/openauth/ui/password"
+import { object, string } from "valibot"
+import { createSubjects } from "@openauthjs/openauth/subject"
+
+const subjects = createSubjects({
+  user: object({
+    id: string(),
+  }),
+})
+
+async function getUser(email: string) {
+  // Get user from database
+  // Return user ID
+  return "123"
+}
+
+export default issuer({
+  subjects,
+  storage: MemoryStorage({
+    persist: "./persist.json",
+  }),
+  providers: {
+    password: PasswordProvider(
+      PasswordUI({
+        sendCode: async (email, code) => {
+          console.log(email, code)
+        },
+      })
+    ),
+  },
+  async allow() {
+    return true
+  },
+  success: async (ctx, value) => {
+    if (value.provider === "password") {
+      return ctx.subject("user", {
+        id: await getUser(value.email),
+      })
+    }
+    throw new Error("Invalid provider")
+  },
+})
