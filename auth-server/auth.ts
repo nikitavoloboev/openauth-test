2ca0387e3209930f06717100ec64749a5207b1af
Nikita <github@nikiv.dev>
2025-01-11 01:01:12 +0100

    google oauth

--- auth-server/auth.ts
+++ auth-server/auth.ts
@@ -2,11 +2,10 @@
 // as per docs: https://openauth.js.org/docs
 // TODO: deploy this on docker container, sst or railway
 import { issuer } from "@openauthjs/openauth"
+import { GoogleProvider } from "@openauthjs/openauth/provider/google"
 import { MemoryStorage } from "@openauthjs/openauth/storage/memory"
-import { PasswordProvider } from "@openauthjs/openauth/provider/password"
-import { PasswordUI } from "@openauthjs/openauth/ui/password"
-import { object, string } from "valibot"
 import { createSubjects } from "@openauthjs/openauth/subject"
+import { object, string } from "valibot"
 
 const subjects = createSubjects({
   user: object({
@@ -14,36 +13,35 @@ const subjects = createSubjects({
   }),
 })
 
-async function getUser(email: string) {
-  // Get user from database
-  // Return user ID
-  return "123"
-}
-
 export default issuer({
   subjects,
   // TODO: change to persistent storage
   storage: MemoryStorage({
     persist: "./persist.json",
   }),
+  // just having google oauth for now
   providers: {
-    password: PasswordProvider(
-      PasswordUI({
-        sendCode: async (email, code) => {
-          console.log(email, code)
-        },
-      })
-    ),
+    google: GoogleProvider({
+      clientID: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      scopes: ["email", "profile"],
+    }),
   },
   async allow() {
     return true
   },
+  // TODO: how should success look for google oauth
   success: async (ctx, value) => {
-    if (value.provider === "password") {
+    if (value.provider === "google") {
       return ctx.subject("user", {
-        id: await getUser(value.email),
+        id: await getUser(value.tokenset.email),
       })
     }
     throw new Error("Invalid provider")
   },
 })
+
+// TODO: how should this function look?
+async function getUser(email: string) {
+  return "123"
+}
